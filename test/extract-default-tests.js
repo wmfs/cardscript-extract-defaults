2a6770036f761e8416366401b76380729ec8429e
meenahoda <meena.hoda@wmfs.net>
2024-05-02 14:04:39 +0100

    update tests

--- test/extract-default-tests.js
+++ test/extract-default-tests.js
@@ -24,184 +24,107 @@ describe('Run some Cardscript default-extracting tests', function () {
     const result = await extractDefaults(complex)
     expect(result).to.eql(
       {
-        cardLists: {},
         rootView: {
           $CARD_DATA_HAS_CHANGED: false,
           $GEOLOCATION: null,
-          base: 'TOMATO',
-          deliveryOrCollection: 'COLLECT',
-          dietaryReq: [],
-          dietaryReqOther: null,
           name: null,
+          phoneNumber: null,
+          dietaryReq: [],
+          dietaryReqSelectedChoices: [],
+          dietaryReqOther: null,
+          size: 'M',
+          sizeSelectedChoices: [],
+          savouryOrSweet: null,
+          savouryOrSweetSelectedChoices: [],
+          base: 'TOMATO',
+          baseSelectedChoices: [],
           hot: false,
           howHot: null,
-          phoneNumber: null,
+          howHotSelectedChoices: [],
+          toppings: [],
+          toppingsSelectedChoices: [],
           primaryFlavour: null,
-          savouryOrSweet: null,
+          primaryFlavourSelectedChoices: [],
           secondaryFlavour: null,
-          size: 'M',
+          secondaryFlavourSelectedChoices: [],
           sprinkles: false,
-          toppings: []
-        }
+          deliveryOrCollection: 'COLLECT',
+          deliveryOrCollectionSelectedChoices: []
+        },
+        cardLists: {}
       }
     )
   })
 
   it('should extract some defaults from kitchen sink example', async function () {
     const result = await extractDefaults(kitchenSink)
-    expect(result).to.eql({
-      rootView: {
-        $CARD_DATA_HAS_CHANGED: false,
-        $GEOLOCATION: null,
-        cardList: [],
-        cardListNonEditable: [],
-        toggle: false,
-        tabSetTabSet: 'tab-0',
-        tabSetTabSetShowWhens: [
-          undefined,
-          undefined
-        ],
-        choice: 'CHOICE_1',
-        choiceMulti: ['CHOICE_1', 'CHOICE_2'],
-        choiceWithTitle: 'CHOICE_1',
-        date: null,
-        dateTime: null,
-        dateTimeSeconds: null,
-        email: null,
-        slider: 3,
-        tableWithMultiSelectionSelected: [],
-        tableWithSingleSelectionSelected: [],
-        text: null,
-        textMultiline: 'isMultiline: true',
-        time: null,
-        textEditor: 'editor: true',
-        inputAddressSearchResults: [],
-        inputSignatureOpenModal: false,
-        inputTelephoneNumber: null,
-        listMultiSelection: [],
-        listSingleSelection: {},
-        markdownTextBlockCompiledMarkdown: '<h2 id="markdown">Markdown</h2>\n<blockquote>\n<p>This is some markdown text</p>\n</blockquote>\n<h2 id="heading">Heading</h2>\n<p>Some text here...</p>\n<ul>\n<li>one</li>\n<li>two</li>\n<li>three</li>\n</ul>\n',
-        name: null,
-        number: null,
-        numberWithMinMax: null,
-        inputApiLookup: {
-          loading: false,
-          params: {
-            searchQuery: null,
-            sortBy: 'MOST_RECENT'
+    expect(result).to.eql(
+      {
+        rootView: {
+          $CARD_DATA_HAS_CHANGED: false,
+          $GEOLOCATION: null,
+          cardList: [],
+          cardListNonEditable: [],
+          inputAddressSearchResults: [],
+          inputApiLookup: {
+            loading: false,
+            summary: { totalHits: 0 },
+            results: [],
+            pagination: { page: 1, totalPages: 0, offset: 0, limit: 10 },
+            params: { searchQuery: null, sortBy: 'MOST_RECENT', sortBySelectedChoices: [] }
           },
-          results: [],
-          pagination: {
-            page: 1,
-            totalPages: 0,
-            limit: 10,
-            offset: 0
-          },
-          summary: {
-            totalHits: 0
-          }
+          choice: 'CHOICE_1',
+          choiceSelectedChoices: [],
+          choiceWithTitle: 'CHOICE_1',
+          choiceWithTitleSelectedChoices: [],
+          choiceMulti: ['CHOICE_1', 'CHOICE_2'],
+          choiceMultiSelectedChoices: [],
+          date: null,
+          dateTime: null,
+          dateTimeSeconds: null,
+          email: null,
+          name: null,
+          number: null,
+          numberWithMinMax: null,
+          inputSignatureOpenModal: false,
+          slider: 3,
+          inputTelephoneNumber: null,
+          text: null,
+          textMultiline: 'isMultiline: true',
+          textEditor: 'editor: true',
+          time: null,
+          toggle: false,
+          listSingleSelection: {},
+          listMultiSelection: [],
+          tableWithSingleSelectionSelected: [],
+          tableWithMultiSelectionSelected: [],
+          tabSetTabSet: 'tab-0',
+          tabSetTabSetShowWhens: [undefined, undefined],
+          markdownTextBlockCompiledMarkdown: '<h2 id="markdown">Markdown</h2>\n<blockquote>\n<p>This is some markdown text</p>\n</blockquote>\n<h2 id="heading">Heading</h2>\n<p>Some text here...</p>\n<ul>\n<li>one</li>\n<li>two</li>\n<li>three</li>\n</ul>\n',
+          treeSimple: [{
+            label: 'Vehicle',
+            children: [{ label: 'Car' }, { label: 'Train' }, { label: 'Bus' }]
+          }, { label: 'Building', children: [{ label: 'House' }, { label: 'Flat' }] }],
+          treeSingle: [{
+            label: 'Vehicle',
+            children: [{ label: 'Car', value: 'CAR' }, { label: 'Train', value: 'TRAIN' }, {
+              label: 'Bus',
+              value: 'BUS'
+            }]
+          }, { label: 'Building', children: [{ label: 'House', value: 'HOUSE' }, { label: 'Flat', value: 'FLAT' }] }],
+          treeSingleSelected: {},
+          treeMulti: [{
+            label: 'Vehicle',
+            children: [{ label: 'Car', value: 'CAR' }, { label: 'Train', value: 'TRAIN' }, {
+              label: 'Bus',
+              value: 'BUS'
+            }]
+          }, { label: 'Building', children: [{ label: 'House', value: 'HOUSE' }, { label: 'Flat', value: 'FLAT' }] }],
+          treeMultiSelected: []
         },
-        treeMulti: [
-          {
-            children: [
-              {
-                label: 'Car',
-                value: 'CAR'
-              },
-              {
-                label: 'Train',
-                value: 'TRAIN'
-              },
-              {
-                label: 'Bus',
-                value: 'BUS'
-              }
-            ],
-            label: 'Vehicle'
-          },
-          {
-            children: [
-              {
-                label: 'House',
-                value: 'HOUSE'
-              },
-              {
-                label: 'Flat',
-                value: 'FLAT'
-              }
-            ],
-            label: 'Building'
-          }
-        ],
-        treeMultiSelected: [],
-        treeSimple: [
-          {
-            children: [
-              {
-                label: 'Car'
-              },
-              {
-                label: 'Train'
-              },
-              {
-                label: 'Bus'
-              }
-            ],
-            label: 'Vehicle'
-          },
-          {
-            children: [
-              {
-                label: 'House'
-              },
-              {
-                label: 'Flat'
-              }
-            ],
-            label: 'Building'
-          }
-        ],
-        treeSingle: [
-          {
-            children: [
-              {
-                label: 'Car',
-                value: 'CAR'
-              },
-              {
-                label: 'Train',
-                value: 'TRAIN'
-              },
-              {
-                label: 'Bus',
-                value: 'BUS'
-              }
-            ],
-            label: 'Vehicle'
-          },
-          {
-            children: [
-              {
-                label: 'House',
-                value: 'HOUSE'
-              },
-              {
-                label: 'Flat',
-                value: 'FLAT'
-              }
-            ],
-            label: 'Building'
-          }
-        ],
-        treeSingleSelected: {}
-      },
-      cardLists: {
-        cardList: {
-          opinion: 'Amazing!'
-        },
-        cardListNonEditable: {}
+        cardLists: { cardList: { opinion: 'Amazing!' }, cardListNonEditable: {} }
       }
-    })
+    )
   })
 
   xit('should extract some defaults from cardList example', async function () {
